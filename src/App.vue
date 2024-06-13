195b93e1da92db06e12cc9915d22a5e592df5950
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-06-13 15:59:33 +0200

    print image and paginate result

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <h1 class="text-center">Ciao</h1>
   <i class="fa fa-solid fa-home"></i>
+  <button @click="getAllProject()" class="btn btn-primary">Cambia pagina</button>
+
   <ul>
     <li v-for="project in projects" :key="project.id">
       <h1>{{ project.title }}</h1>
+      <img :src="store.imgBasePath + project.image" :alt="project.title">
       <p v-html="project.description"></p>
     </li>
   </ul>
@@ -18,14 +21,18 @@ export default {
   data() {
     return {
       store,
-      projects: []
+      projects: [],
+      currentPage: 0,
+      nextPage: 0
     }
   },
   methods: {
     getAllProject() {
-      axios.get(this.store.apiBaseUrl + '/projects').then((res) => {
+      this.nextPage = this.currentPage + 1;
+      axios.get(this.store.apiBaseUrl + '/projects', { params: { page: this.nextPage } }).then((res) => {
         console.log(res.data);
-        this.projects = res.data.results;
+        this.projects = res.data.results.data;
+        this.currentPage = res.data.results.current_page;
       })
     }
   },
@@ -35,4 +42,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+li img {
+  width: 500px;
+}
+</style>
